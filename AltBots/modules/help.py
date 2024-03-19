3ba3031f10ffe0803ce4a7f7c2ba6db49f5973d5
Ezios$709 <158480426+ITACHI9264@users.noreply.github.com>
2024-03-19 12:36:22 +0530

    Update help.py

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ 𝙓𝘽𝙤𝙩𝙨 𝙃𝙚𝙡𝙥 𝙈𝙚𝙣𝙪 ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @Broken_villen**"
+HELP_STRING = f"★ 𝗩𝗲𝗴𝗶𝘁𝗼 𝙃𝙚𝙡𝙥 𝙈𝙚𝙣𝙪 ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @Broken_villen**"
 
 HELP_BUTTON = [
     [
